f5199ae279c32349eefe20d1682d7f8ced676c6a
mlarmet <maxencelarmet@orange.fr>
2022-01-10 20:22:29 +0100

    javadoc + verif setter

--- Evenement.java
+++ Evenement.java
@@ -4,24 +4,24 @@ import java.time.LocalDateTime;
  * Description de la classe evenement.
  */
 public class Evenement {
-  /**Description Nom de l evenement.*/
+  /** Description Nom de l evenement. */
   private String nom;
-  /**Description description de l'evenement.*/
+  /** Description description de l'evenement. */
   private String descriptif;
-  /**Description image de profile de l'evenement.*/
+  /** Description image de profile de l'evenement. */
   private String image;
-  /**Description date et horraire de l'evenement.*/
+  /** Description date et horraire de l'evenement. */
   private LocalDateTime dateHoraire;
-  /**Description lieu de l'evenement.*/
+  /** Description lieu de l'evenement. */
   private Lieu lieu;
-  /**Description type de l'evenement.*/
+  /** Description type de l'evenement. */
   private TypeEvenement type;
-  /**Description nombre maximum de personne dans l'evenement.*/
+  /** Description nombre maximum de personne dans l'evenement. */
   private int nombreMaxPersonne;
 
   /**
    * Constructeur de la classe Evenement.
-
+   *
    * @param nom nom de l'evenement
    * @param descriptif description de l'evenement
    * @param image image de profile de l'evenement
@@ -45,56 +45,128 @@ public class Evenement {
     return nom;
   }
 
-  public void setNom(String nom) {
+  /**
+   * Modifie le nom de l'evenement.
+   *
+   * @param nom nouveau nom de l'evenement
+   * @return -1 si le nouveau nom est vide, 1 si la modification est correct
+   */
+  public int setNom(String nom) {
+    if (nom.isBlank() || nom.isEmpty()) {
+      return -1;
+    }
+
     this.nom = nom;
+    return 1;
   }
 
   public String getDescriptif() {
     return descriptif;
   }
 
-  public void setDescriptif(String descriptif) {
+  /**
+   * Modifie la description de l'evenement.
+   *
+   * @param descriptif nouvelle description de l'evenement
+   * @return -1 si la nouvelle description est vide, 1 si la modification est correct
+   */
+  public int setDescriptif(String descriptif) {
+    if (descriptif.isBlank() || descriptif.isEmpty()) {
+      return -1;
+    }
+
     this.descriptif = descriptif;
+
+    return 1;
   }
 
   public String getImage() {
     return image;
   }
 
-  public void setImage(String image) {
+  /**
+   * Modifie le nom de l'image de l'evenement.
+   *
+   * @param image nouveau nom de l'image de l'evenement
+   * @return -1 si le nouveau est nom est vide, 1 si la modification est correct
+   */
+  public int setImage(String image) {
+    if (image.isBlank() || image.isEmpty()) {
+      return -1;
+    }
+
     this.image = image;
+    return 1;
   }
 
   public LocalDateTime getDateHoraire() {
     return dateHoraire;
   }
 
-  public void setDateHoraire(LocalDateTime dateHoraire) {
+  /**
+   * Modifie la date et l'horraire de l'image de l'evenement.
+   *
+   * @param dateHoraire nouveau nom de l'image de l'evenement
+   * @return -1 si la nouvelle date est inférieur a la date actuelle, 1 si la modification est
+   *         correct
+   */
+  public int setDateHoraire(LocalDateTime dateHoraire) {
+    if (dateHoraire.isBefore(LocalDateTime.now())) {
+      return -1;
+    }
+
     this.dateHoraire = dateHoraire;
+    return 1;
   }
 
   public Lieu getLieu() {
     return lieu;
   }
 
-  public void setLieu(Lieu lieu) {
+  /**
+   * Modifie le lieu de l'evenement.
+   *
+   * @param lieu nouveau lieu de l'evenement
+   * @return -1 si le nouveau lieu est incorrect, 1 si la modification est correct
+   */
+  public int setLieu(Lieu lieu) {
+    // TODO test lieu incorrect ??
     this.lieu = lieu;
+    return 1;
   }
 
   public TypeEvenement getType() {
     return type;
   }
 
-  public void setType(TypeEvenement type) {
+  /**
+   * Modifie le type de l'evenement.
+   *
+   * @param type nouveau type de l'evenement
+   * @return -1 si le nouveau type est incorrect, 1 si la modification est correct
+   */
+  public int setType(TypeEvenement type) {
+    // TODO test type incorrect
     this.type = type;
+    return 1;
   }
 
   public int getNombreMaxPersonne() {
     return nombreMaxPersonne;
   }
 
-  public void setNombreMaxPersonne(int nombreMaxPersonne) {
+  /**
+   * Modifie le nombre maximum de personne à l'evenement.
+   *
+   * @param nombreMaxPersonne nouveau nombre max de personne de l'evenement
+   * @return -1 si le nouveau nombre est negatif ou égale à 0, 1 si la modification est correct
+   */
+  public int setNombreMaxPersonne(int nombreMaxPersonne) {
+    if (nombreMaxPersonne < 1) {
+      return -1;
+    }
     this.nombreMaxPersonne = nombreMaxPersonne;
+    return 1;
   }
 
 
--- Membres.java
+++ Membres.java
@@ -1,28 +1,29 @@
 import java.util.Date;
 
-/**Class membre.
+/**
+ * Class membre.
  */
 public class Membres {
-  /**Description nom du membre.*/
+  /** Description nom du membre. */
   private String nom;
-  /**Description prenom du membre.*/
+  /** Description prenom du membre. */
   private String prenom;
-  /**Description adresse mail du membre.*/
+  /** Description adresse mail du membre. */
   private String adresseMail;
-  /**Description pseudo du membre.*/
+  /** Description pseudo du membre. */
   private String pseudo;
-  /**Description mot de passe du membre.*/
+  /** Description mot de passe du membre. */
   private String motDePasse;
-  /**Description adresse postale du membre.*/
+  /** Description adresse postale du membre. */
   private String adressePostale;
-  /**Description date de naissance du membre.*/
+  /** Description date de naissance du membre. */
   private Date dateNaissance;
-  /**Description lieu de naissance du membre.*/
+  /** Description lieu de naissance du membre. */
   private String lieuNaissance;
 
   /**
    * Constructeur de la classe Membres.
-
+   *
    * @param nom nom du membre
    * @param prenom prenom du membre
    * @param adresseMail adresse email du membre
@@ -49,64 +50,148 @@ public class Membres {
     return nom;
   }
 
-  public void setNom(String nom) {
+  /**
+   * Modifie la description de l'evenement.
+   *
+   * @param nom nouveau nom du membre
+   * @return -1 si le nouveau nom est vide, 1 si la modification est correct
+   */
+  public int setNom(String nom) {
+    if (nom.isBlank() || nom.isEmpty()) {
+      return -1;
+    }
     this.nom = nom;
+    return 1;
   }
 
   public String getPrenom() {
     return prenom;
   }
 
-  public void setPrenom(String prenom) {
+  /**
+   * Modifie le prenom du membre.
+   *
+   * @param prenom nouveau prenom du membre
+   * @return -1 si le nouveau prenom est vide, 1 si la modification est correct
+   */
+  public int setPrenom(String prenom) {
+    if (prenom.isBlank() || prenom.isEmpty()) {
+      return -1;
+    }
     this.prenom = prenom;
+    return 1;
   }
 
   public String getAdresseMail() {
     return adresseMail;
   }
 
-  public void setAdresseMail(String adresseMail) {
+  /**
+   * Modifie l'adresse email du membre.
+   *
+   * @param adresseMail nouvel emai du membre
+   * @return -1 si le nouvel email est vide, 1 si la modification est correct
+   */
+  public int setAdresseMail(String adresseMail) {
+    if (adresseMail.isBlank() || adresseMail.isEmpty()) {
+      return -1;
+    }
     this.adresseMail = adresseMail;
+    return 1;
   }
 
   public String getPseudo() {
     return pseudo;
   }
 
-  public void setPseudo(String pseudo) {
+  /**
+   * Modifie le pseudo du membre.
+   *
+   * @param pseudo nouveau pseudo du membre
+   * @return -1 si le nouveau pseudo est vide, 1 si la modification est correct
+   */
+  public int setPseudo(String pseudo) {
+    if (pseudo.isBlank() || pseudo.isEmpty()) {
+      return -1;
+    }
     this.pseudo = pseudo;
+    return 1;
   }
 
   public String getMotDePasse() {
     return motDePasse;
   }
 
-  public void setMotDePasse(String motDePasse) {
+  /**
+   * Modifie le mot de passe du membre.
+   *
+   * @param motDePasse nouveau mot de passe du membre
+   * @return -1 si le nouveau mot de passe est vide, 1 si la modification est correct
+   */
+  public int setMotDePasse(String motDePasse) {
+    if (motDePasse.isBlank() || motDePasse.isEmpty()) {
+      return -1;
+    }
     this.motDePasse = motDePasse;
+    return 1;
   }
 
   public String getAdressePostale() {
     return adressePostale;
   }
 
-  public void setAdressePostale(String adressePostale) {
+  /**
+   * Modifie l'adresse postal du membre.
+   *
+   * @param adressePostale nouvelle adresse du membre
+   * @return -1 si la nouvelle adresse est vide, 1 si la modification est correct
+   */
+  public int setAdressePostale(String adressePostale) {
+    if (adressePostale.isBlank() || adressePostale.isEmpty()) {
+      return -1;
+    }
     this.adressePostale = adressePostale;
+    return 1;
   }
+
 
   public Date getDateNaissance() {
     return dateNaissance;
   }
 
-  public void setDateNaissance(Date dateNaissance) {
+  /**
+   * Modifie la date de naissance du membre.
+   *
+   * @param dateNaissance nouvelle date de naissance du membre
+   * @return -1 si la nouvelle date est supérieur à la date actuelle, 1 si la modification est
+   *         correct
+   */
+  public int setDateNaissance(Date dateNaissance) {
+    // si sup à 0 alors dateNaissance est supérieur à aujourd'hui.
+    if (dateNaissance.compareTo(new Date()) > 0) {
+      return -1;
+    }
+
     this.dateNaissance = dateNaissance;
+    return 1;
   }
 
   public String getLieuNaissance() {
     return lieuNaissance;
   }
 
-  public void setLieuNaissance(String lieuNaissance) {
+  /**
+   * Modifie le lieu de naissance du membre.
+   *
+   * @param lieuNaissance nouveau lieu de naissance du membre
+   * @return -1 si le nouveau lieu est vide, 1 si la modification est correct
+   */
+  public int setLieuNaissance(String lieuNaissance) {
+    if (lieuNaissance.isBlank() || lieuNaissance.isEmpty()) {
+      return -1;
+    }
     this.lieuNaissance = lieuNaissance;
+    return 1;
   }
 
 
